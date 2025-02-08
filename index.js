132ff9145dbe8177c0994cde3aa0c6deb6c049bd
CleitonQ <cleitonqueirozc@gmail.com>
2025-02-07 21:46:32 -0300

    Update 07022025 1

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-var elementosDuvida = document.querySelectorAll('.duvida')
+var elementosDuvida = document.querySelectorAll('.duvida');
 
-elementosDuvida.forEach(function (duvida){
-    duvida.addEventListener('click', function(){
-        duvida.classList.toggle('ativa')
-    })
-})
+elementosDuvida.forEach(function (duvida) {
+    duvida.querySelector('h3').addEventListener('click', function () {
+        duvida.classList.toggle('ativa');
+        const paragrafo = duvida.querySelector('.paragrafo');
+        if (duvida.classList.contains('ativa')) {
+            paragrafo.style.height = 'fit-content';
+            paragrafo.style.opacity = '1';
+            paragrafo.style.padding = '24px';
+        } else {
+            paragrafo.style.height = '0';
+            paragrafo.style.opacity = '0';
+            paragrafo.style.padding = '0';
+        }
+    });
+});
